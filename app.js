cfa1cd8f8879c28568b3b6e0c4d56af0f8863506
Mursalin <atifmursalin4@gmail.com>
2021-05-09 11:37:46 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const textarea = document.querySelector('textarea');
+const count = document.querySelector('.count');
+
+textarea.addEventListener('keyup', function(){
+    const text = textarea.value.trim();
+    
+});
